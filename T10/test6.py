cdd8896b749f483669894a5f82c34332587fe9cf
Elvira-max17 <vil.sulkiyeva@mail.ru>
2025-06-05 14:09:03 +0300

    Update test6.py

--- T10/test6.py
+++ T10/test6.py
@@ -1,1 +1,20 @@
+import random
+import time
+from memory_profiler import profile
 
+@profile
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+if __name__ == "__main__":
+    arr_size = 10000000
+    arr = sorted([random.randint(1, arr_size) for _ in range(arr_size)])  # Сгенерирован отсортированный массив
+    target = arr[-1]  # Поиск последнего элемента
+
+    start_time = time.time()
+    linear_search(arr, target)
+    end_time = time.time()
+    print(f"Время выполнения линейного поиска: {end_time - start_time:.4f} секунд")
